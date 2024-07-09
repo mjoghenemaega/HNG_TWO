1ee256dbb45dd5e00a57cbfd44a63b47b5980206
mjoghenemaega <mjoghenemaega@gmail.com>
2024-07-09 11:15:38 +0100

    set for vercel

--- Userapp/urls.py
+++ Userapp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('api/users/<uuid:userId>', UserDetailView.as_view(), name='user-detail'),
     path('api/organisations', OrganisationListCreateView.as_view(), name='organisation-list'),
     path('api/organisations/<uuid:orgId>', OrganisationDetailView.as_view(), name='organisation-detail'),
-    path('api/organisations/<uuid:orgId>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation')
+    path('api/organisations/<uuid:orgId>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
+    path('api/auth/register', RegisterView.as_view(), name='register3'),
+    path('api/auth/login', LoginView.as_view(), name='login'),
 ]
